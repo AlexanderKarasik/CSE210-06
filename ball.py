d6bd26b11b1b1c1975a9d6686949ffb8adea1117
AlexanderKarasik <97298711+AlexanderKarasik@users.noreply.github.com>
2022-04-01 22:32:11 +0300

    Update ball.py

--- ball.py
+++ ball.py
@@ -1,7 +1,5 @@
 import pygame
-
-
-WIDTH, HEIGHT = 900, 500 # add constants for update 
+from constants import *
 
 class Ball:
     def __init__(self, screen, color, posX, posY, radius):
@@ -12,30 +10,30 @@ class Ball:
         self.dx = 0
         self.dy = 0
         self.radius = radius
-        self.draw()
+        self._draw()
 	
 		
-    def draw(self):
+    def _draw(self):
         pygame.draw.circle( self.screen, self.color, (self.posX, self.posY), self.radius )
 
-    def start(self):
+    def _start(self):
 		# this will be random
         self.dx = 15
         self.dy = 5
 
-    def move(self):
+    def _move(self):
         self.posX += self.dx
         self.posY += self.dy
 
-    def wall_collision(self):
+    def _wall_collision(self):
         self.dy = -self.dy
 
-    def paddle_collision(self):
+    def _paddle_collision(self):
         self.dx = -self.dx
 
-    def restart_pos(self):
+    def _restart_pos(self):
         self.posX = WIDTH//2
         self.posY = HEIGHT//2
         self.dx = 0
         self.dy = 0
-        self.draw()
+        self._draw()
